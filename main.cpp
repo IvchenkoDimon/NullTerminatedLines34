66a75bb8bf70a62c3864058a921c633b7ed20913
Dmitry <87kinglion87@gmail.com>
2019-05-30 19:05:50 +0300

    HexToDec ternary

--- main.cpp
+++ main.cpp
@@ -2,21 +2,25 @@
 #include<Windows.h>
 using namespace std;
 
+//#define CASE_FUNCTIONS
+
+
 void InputLine(char szStr[], const int n);
-int  StrLen(char szStr[]);
+int  StrLen(char szStr[]);		//Показывает количество символов в строке.
 void UpperCase(char szStr[]);	//Переводит строку в верхний регистр.
 void LowerCase(char szStr[]);	//Переводит строку в нижний  регистр.
 void Capitalize(char szStr[]);	//Первую букву каждого слова в предложении делает заглавной.
 bool isLowLetter(char symbol);
 bool isLowRusLetter(char symbol);
 
-void Shrink(char szStr[]);			//Удаляет лишние пробелы между словами в предложении.
+void Shrink(char szStr[]);		//Удаляет лишние пробелы между словами в предложении.
 void RemoveSpaces(char szStr[]);
 bool isPalindrome(char szStr[]);	//Проверяет, является ли данная строка палиндромом.
 bool isNumber(char szStr[]);	//Проверяет, является строка числом. Строка является числом, если содержит только цифры.
-int StringToInt(char szStr[]);
 bool isHexNumber(char szStr[]);	//Проверяет, является строка шестнадцатеричным числом. Строка является Hex-числом, если содержит только цифры и буквы ABCDEF либо abcdef.
-
+bool isBinNumber(char szStr[]);
+int  Bin2Dec(char szStr[]);
+int  Hex2Dec(char szStr[]);
 
 void main()
 {
@@ -35,17 +39,24 @@ void main()
 	//InputLine(szStr, n);
 	//cin >> szStr;
 	cin.getline(szStr, n);
-	//cout << szStr << endl;
-	//cout << sizeof(szStr) << endl;
-	/*UpperCase(szStr);
+	cout << szStr << endl;
+#ifdef CASE_FUNCTIONS
+	cout << sizeof(szStr) << endl;
+	UpperCase(szStr);
 	cout << szStr << endl;
 	Capitalize(szStr);
-	cout << szStr << endl;*/
-	Shrink(szStr);
 	cout << szStr << endl;
+#endif // CAS
+
+	//Shrink(szStr);
+	//cout << szStr << endl;
 	//cout << isPalindrome(szStr) << endl;
-	cout << isNumber(szStr) << endl;
-	cout << StringToInt(szStr) << endl;
+	//cout << szStr << endl;
+	cout << "Введенное значение число?: "  << isNumber(szStr) << endl;
+	cout << "Введенное значение HEX число?: " << isHexNumber(szStr) << endl;
+	cout << "Введенное значение BIN число?: " << isBinNumber(szStr) << endl;
+	cout << "BinToDec: " << Bin2Dec(szStr) << endl;
+	cout << "HexToDec: " << Hex2Dec(szStr) << endl;
 }
 
 void InputLine(char szStr[], const int n)
@@ -53,7 +64,7 @@ void InputLine(char szStr[], const int n)
 	//cout << typeid(szStr).name() << endl;
 	SetConsoleCP(1251);
 	cin.getline(szStr, n);
-	SetConsoleCP(866);
+	SetConsoleCP(867);
 }
 int  StrLen(char szStr[])
 {
@@ -129,16 +140,15 @@ void RemoveSpaces(char szStr[])
 }
 bool isPalindrome(char szStr[])
 {
-
 	int size = StrLen(szStr);
 	char* buffer = new char[size + 1]{};
 	for (int i = 0; i < size; i++)buffer[i] = szStr[i];
-	RemoveSpaces(buffer);
 	LowerCase(buffer);
+	RemoveSpaces(buffer);
 	size = StrLen(buffer);
 	for (int i = 0; i < size / 2; i++)
 	{
-		if (buffer[i] != buffer[size - i - 1])
+		if (buffer[i] != buffer[size - 1 - i])
 		{
 			delete[] buffer;
 			return false;
@@ -149,33 +159,117 @@ bool isPalindrome(char szStr[])
 }
 bool isNumber(char szStr[])
 {
-	if ((szStr[0]<'0' || szStr[0] > '9') && szStr[0] != '-' && szStr[0] != '+')
-		return false;
+	if ((szStr[0] < '0' || szStr[0] > '9') && szStr[0] != '-' && szStr[0] != '+')return false;
 	for (int i = 1; szStr[i]; i++)
 	{
-		if (szStr[i]<'0' || szStr[i] > '9')
+		if (szStr[i] < '1' || szStr[i] > '9') return false;
+	return true;
+	}
+}
+bool isHexNumber(char szStr[])
+{
+	for (int i = 0; szStr[i]; i++)
+	{
+		if (
+			!(szStr[i] >= '0' && szStr[i] <= '9') &&
+			!(szStr[i] >= 'A' && szStr[i] <= 'F') &&
+			!(szStr[i] >= 'a' && szStr[i] <= 'f')
+			)
+		{
 			return false;
+		}
 	}
 	return true;
 }
-int StringToInt(char szStr[])
+bool isBinNumber(char szStr[])
 {
-	if (!isNumber(szStr))return 0;
-	int decimal = 0;
-	int signed_number = false;
-	if (szStr[0] == '+' || szStr[0] == '-')signed_number = true;
-	for (int i = signed_number ? 1 : 0; szStr[i]; i++)
+	int n = StrLen(szStr);
+	char* buffer = new char[n + 1]{};
+	for (int i = 0; szStr[i]; i++)buffer[i] = szStr[i];
+	RemoveSpaces(buffer);
+	for (int i = 0; buffer[i]; i++)
 	{
-		decimal *= 10;
-		decimal += szStr[i] - 48;
+		if (buffer[i] != '0' && buffer[i] != '1')
+		{
+			delete[] buffer;
+			return false;
+		}
 	}
-	if (szStr[0] == '-')decimal = -decimal;
-	return decimal;
+	delete[] buffer;
+	return true;
 }
-//bool isHexNumber(char szStr[])
-//{
-//	for (int i = 0; szStr[i]; i++)
-//	{
-//		if ((szStr[i]<'0' || szStr[i] > '9') && szStr[i] >= 'a')return false;
-//	}
-//}
+int  Bin2Dec(char szStr[])
+{
+	if (!isBinNumber(szStr))
+	{
+		cout << "Error: not binary" << endl;
+		return 0;
+	}
+	int capacity = StrLen(szStr);
+	int Dec = 0; //Десятичное число;
+	int weight = 1; //Весовой коэфициэнт двоичного числа;
+	for (int i = capacity-1; i>=0; i--)
+	{
+		if (szStr[i] == ' ') continue;
+		Dec += (szStr[i] - 48)*weight;
+		weight *= 2;
+	}
+	return Dec;
+}
+int  Hex2Dec(char szStr[])
+{
+	if (!isHexNumber(szStr))
+	{
+		cout << "Error: not HEX" << endl;
+		return 0;
+	}
+	// Вариант из интернета;
+	/*int	digit;
+	int Dec = 0;
+	int capacity = strlen(szStr) - 1;
+	for (int i = 0; szStr[i] != '\0'; i++)
+	{
+		switch (toupper(szStr[i]))
+		{
+		case 'A': digit = 10; break;
+		case 'B': digit = 11; break;
+		case 'C': digit = 12; break;
+		case 'D': digit = 13; break;
+		case 'E': digit = 14; break;
+		case 'F': digit = 15; break;
+		case '1': digit = 1; break;
+		case '2': digit = 2; break;
+		case '3': digit = 3; break;
+		case '4': digit = 4; break;
+		case '5': digit = 5; break;
+		case '6': digit = 6; break;
+		case '7': digit = 7; break;
+		case '8': digit = 8; break;
+		case '9': digit = 9; break;
+		case '0': digit = 0; break;
+		}
+		Dec = Dec + digit * pow(16, capacity);
+		capacity--;
+	}
+	return Dec;*/ 
+	// Вариант 1
+	int Dec = 0;
+	int weight = 1;
+	UpperCase(szStr);
+	for (int i = StrLen(szStr) - 1; i >= 0; i--)
+	{
+		/*int digit;
+		if (szStr[i] >= 'a')digit = szStr[i] - 87;
+		else if (szStr[i] >= 'A')digit = szStr[i] - 55;
+		else digit = szStr[i] - 48;
+		Dec += digit*weight;*/
+		//Вариант 2
+		/*Dec+=
+		(szStr[i] >= 'a' ? szStr[i] - 87 : szStr[i] >= 'A' ? szStr[i] - 55 : szStr[i] - 48)*weight;*/
+		//Вариант 3
+		Dec += (szStr[i] >= 'A' ? szStr[i] - 55 : szStr[i] - 48)*weight;
+		weight *= 16;
+	}
+	return Dec;
+}
+
